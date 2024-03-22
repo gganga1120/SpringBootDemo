4da471ca6888c6899d8444a04f2c4566adec144b
Ganga Ganapathi Gajulapati <59639073+ggajulapati@users.noreply.github.com>
2024-03-22 18:58:35 +0530

    first commit

---
+++ src/main/java/com/osi/curd/HomeController.java
@@ -0,0 +1,13 @@
+package com.osi.curd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/gitWelcome")
+	public String gitWelcome() {
+		return "Git welcome";
+	}
+}
